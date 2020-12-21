a364b20fe0b2e4a5d9bdee376662e31b5d41dfe4
Rick Wassing <Mac-n-Cheese@Ricks-MacBook-Pro.local>
2020-12-21 14:55:46 +1100

    Version 0.1.4 (beta)

--- package/appFunc/app_showErrMsg.m
+++ package/appFunc/app_showErrMsg.m
@@ -1,9 +1,23 @@
 function app_showErrMsg(app, ME)
 % ----------------------------------------
+% Close all menu's
+delete(app.importSleepDiaryApp);
+delete(app.editDatasetInfoApp);
+delete(app.selectDataApp);
+delete(app.changeTimeZoneApp);
+delete(app.changeEpochLengthApp);
+delete(app.annotateActivityGGIRApp);
+delete(app.annotateLightApp);
+delete(app.createEventApp);
+delete(app.createDailyEventApp);
+delete(app.createRelativeEventApp);
+delete(app.editEventApp);
+delete(app.selectColorApp);
+% ----------------------------------------
 % Construct error message
 errmsg = {ME.message};
 for si = 1:length(ME.stack)
-    errmsg = [errmsg; {['Error in ', ME.stack(si).name, ' (line ', num2str(ME.stack(si).line) ,')']}];
+    errmsg = [errmsg; {['Error in ', ME.stack(si).name, ' (line ', num2str(ME.stack(si).line) ,')']}]; %#ok<AGROW>
 end
 % ----------------------------------------
 % Show error message
@@ -11,5 +25,5 @@ closeWaitbar()
 sel = uiconfirm(app.CicadaUIFigure, errmsg, 'Error. Well, that is embarrassing...', ...
     'Options',{'Ok'},...
     'DefaultOption', 'Ok', ...
-    'Icon', 'error');
+    'Icon', 'error'); %#ok<NASGU>
 end
